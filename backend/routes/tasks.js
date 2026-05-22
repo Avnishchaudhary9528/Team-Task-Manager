63c519f6f4dd9a9c6023ca1e9d1c9251c791c294
Avnish Chaudhary <avnishchaudhary@MacBook-Air-3.local>
2026-05-22 14:35:11 +0530

    Fix tasks route import error

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {
-  getTasks, getProjectTasks, getTask, createTask, updateTask, deleteTask, addComment, deleteComment,
-} = require('../controllers/taskController');
+const taskController = require('../controllers/taskController');
 const { protect } = require('../middleware/auth');
 
 router.use(protect);
 
-router.route('/').get(getTasks).post(createTask);
-router.get('/project/:projectId', getProjectTasks);
-router.route('/:id').get(getTask).put(updateTask).delete(deleteTask);
-router.post('/:id/comments', addComment);
-router.delete('/:id/comments/:commentId', deleteComment);
+router.route('/').get(taskController.getTasks).post(taskController.createTask);
+router.get('/project/:projectId', taskController.getProjectTasks);
+router.route('/:id').get(taskController.getTask).put(taskController.updateTask).delete(taskController.deleteTask);
+router.post('/:id/comments', taskController.addComment);
+router.delete('/:id/comments/:commentId', taskController.deleteComment);
 
 module.exports = router;
